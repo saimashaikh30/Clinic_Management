702ad61ed6ebd652589c20b38af85795964f5947
saima <saima@SAIMA>
2024-12-01 14:38:28 +0530

    Add project files.

---
+++ Clinic_Management/home.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Clinic_Management
+{
+    partial class home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(388, 300);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(106, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Patient Info";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(609, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 90);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Medicine Inventory";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(812, 300);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 90);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "In-House Details";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1336, 738);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "home";
+            this.Text = "home";
+            this.Load += new System.EventHandler(this.home_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Clinic_Management/home.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic_Management
+{
+    public partial class home : Form
+    {
+        public home()
+        {
+            InitializeComponent();
+        }
+
+        private void home_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
